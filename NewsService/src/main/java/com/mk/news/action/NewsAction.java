c4e126e65f2341f3df52d767b8b3f832b7588056
616164124@qq.com <616164124@qq.com>
2023-05-06 05:16:16 +0800

    2023年4月9日

--- NewsService/src/main/java/com/mk/news/action/NewsAction.java
+++ NewsService/src/main/java/com/mk/news/action/NewsAction.java
@@ -1,8 +1,12 @@
 package com.mk.news.action;
 
+import cn.hutool.core.lang.Snowflake;
+import com.mk.mkcommon.User;
 import com.mk.news.service.NewsService;
+import com.mk.result.BaseBean;
 import com.mk.result.WebResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -13,11 +17,22 @@ public class NewsAction {
     @Resource
     private NewsService newsService;
 
-    @RequestMapping("/dubbo3/news/01")
+    @RequestMapping(value = "/dubbo3/news/01",method = RequestMethod.GET)
     public WebResult test01(){
         WebResult webResult = newsService.news01();
         return webResult;
+    }
 
+
+    @RequestMapping(value = "/dubbo3/news/02",method = RequestMethod.GET)
+    public WebResult test02(){
+        User user = new User();
+        user.setId(new Snowflake().nextIdStr());
+        BaseBean baseBean = new BaseBean();
+        baseBean.setObject(user);
+        baseBean.setType("NEWS02");
+        WebResult webResult =newsService.news02(baseBean);
+        return webResult;
     }
 
 }
--- NewsService/src/main/java/com/mk/news/service/impl/NewsServiceImpl.java
+++ NewsService/src/main/java/com/mk/news/service/impl/NewsServiceImpl.java
@@ -1,6 +1,7 @@
 package com.mk.news.service.impl;
 
 import com.mk.DubboService01;
+import com.mk.DubboService02;
 import com.mk.news.service.NewsService;
 import com.mk.result.BaseBean;
 import com.mk.result.WebResult;
@@ -15,6 +16,9 @@ public class NewsServiceImpl implements NewsService {
     @DubboReference(retries = 1,timeout = 4_000,mock = "",loadbalance = "")
     private DubboService01 dubboService01;
 
+    @DubboReference
+    private DubboService02 dubboService02;
+
     @Override
     public WebResult news01() {
         BaseBean baseBean = new BaseBean();
@@ -22,6 +26,12 @@ public class NewsServiceImpl implements NewsService {
         baseBean.setType("test01");
         log.info("dubboService01=========>前半部分");
         WebResult webResult = dubboService01.dubboService01(baseBean);
+        return webResult;
+    }
+
+    @Override
+    public WebResult news02(BaseBean baseBean) {
+        WebResult webResult = dubboService02.dubboService02(baseBean);
         return webResult;
     }
 }
